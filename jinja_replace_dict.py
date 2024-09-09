9f6b89b1a0e9a1faad5d5b68f10060b1ee1c849f
pritchyspritch <47423802+pritchyspritch@users.noreply.github.com>
2024-09-09 13:25:04 +0100

    Initial commit

---
+++ jinja_replace_dict.py
@@ -0,0 +1,5 @@
+REPLACEMENT_DICT = {'app' : 'DCAP',
+                    'environment' : '',
+                    'version' : '',
+                    'splunk_user' : 'ssphp_app_account',
+                    }
